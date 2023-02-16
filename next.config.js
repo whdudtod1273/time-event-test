23ff8d8882ab3f979c05eca15eac1dfecfd93f98
whdudtod1273 <whdudtod1273@naver.com>
2023-02-16 16:57:02 +0900

    git page 배포 셋팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  assetPrefix: isProd ? "https://whdudtod1273.github.io/time-event-test/" : "",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
