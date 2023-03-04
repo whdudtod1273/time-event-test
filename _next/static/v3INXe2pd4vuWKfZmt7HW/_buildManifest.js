cc304928af378a0fc7cedf071d25847e673dfd63
whdudtod1273 <whdudtod1273@users.noreply.github.com>
2023-03-04 06:48:37 +0000

    deploy: c36ab309852bd996445d19e6383c24eecd2dccce

---
+++ _next/static/v3INXe2pd4vuWKfZmt7HW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c692cee14962ee2.css","static/chunks/pages/index-e6b1647a1041050a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
